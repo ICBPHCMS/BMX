4d7a358245d81d4491e8c6f79eb565209ff06456
matt-komm <Matthias.Komm@cern.ch>
2018-09-28 14:30:29 +0100

    remove generated files

--- Env/env_gpu.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/vols/build/cms/mkomm/BPH/BMX/Env/env/miniconda/bin:$PATH
-source activate tf_gpu
-export TF_CPP_MIN_LOG_LEVEL=2
-export OMP_NUM_THREADS=16 #reduce further if out-of-memory
-ulimit -s unlimited
