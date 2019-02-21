aa48c6cc72c032fbd279b9254f1f3a777d5f32fe
matt-komm <Matthias.Komm@cern.ch>
2019-02-21 16:57:40 +0000

    src env

--- Tests/testUnpacking.sh
+++ Tests/testUnpacking.sh
@@ -2,10 +2,12 @@
 
 function execute() 
 {
+    source Env/env_cpu.sh || return 1
     python Utils/unpackForTraining.py \
     --mc="Tests/samples/*sig*.root" \
     --data="Tests/samples/*bkg*.root" \
     -n 1 -b 0 -o . || return 1
+    source deactivate tf_cpu || return 1
 }
 
 execute
